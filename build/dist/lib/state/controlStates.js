d6efd6eb1b6c4a1f48ab49dae9e9b058747faad9
Daniel Neveux <danil_fransman@hotmail.com>
2017-11-22 00:59:21 +0100

    build

--- build/dist/lib/state/controlStates.js
+++
@@ -1,5 +0,0 @@
-import { registerControlStatePredicate } from "../../vendor/arcanium/lib/state";
-import { CONTROL_STATE } from "../../vendor/arcanium/api/types";
-registerControlStatePredicate(CONTROL_STATE.INITIALIZING, function (model) { return model.initializing; });
-registerControlStatePredicate(CONTROL_STATE.READY, function (model) { return !model.initializing; });
-//# sourceMappingURL=controlStates.js.map
--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const Axios = axios.create({
-    baseURL: process.env.REACT_APP_ROOT_URL || "https://calm-ravine-73007.herokuapp.com/",
+    baseURL: "https://calm-ravine-73007.herokuapp.com/",
     timeout: 30000
 });
