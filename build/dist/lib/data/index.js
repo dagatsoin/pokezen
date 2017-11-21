9624ba5420d0ff34a5b0ec9efd593599ea108b19
Daniel Neveux <danil_fransman@hotmail.com>
2017-11-22 00:44:54 +0100

    other try for server url

--- build/dist/lib/data/index.js
+++ build/dist/lib/data/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export var Axios = axios.create({
-    baseURL: process.env.REACT_APP_ROOT_URL || "https://calm-ravine-73007.herokuapp.com/",
+    baseURL: "https://calm-ravine-73007.herokuapp.com/",
     timeout: 30000
 });
 //# sourceMappingURL=index.js.map
