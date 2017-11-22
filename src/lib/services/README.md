b695b83c622a6484d723fe46a1c2ea5d19409e21
Daniel Neveux <danil_fransman@hotmail.com>
2017-11-22 12:38:32 +0100

    add live tweeter feed

---
+++ src/lib/services/README.md
@@ -0,0 +1,2 @@
+Services look at the state mutations and dispatch optional actions.
+For example, if a new pokemon is stored for being dislayerd in Details.tsx we auto start fetching Tweets about this pokemon.
--- src/lib/services/services.ts
+++ src/lib/services/services.ts
@@ -1,9 +1,25 @@
 import { model } from "../model/index";
-import { IObjectChange, observe } from "mobx";
+import { reaction } from "mobx";
+import { dispatch } from "../../vendor/arcanium/container";
+import { normalize } from "../utils";
+import { clearInterval } from "timers";
+import { Pokemon } from "../../api/types";
 
 /**
  * Services react to the store mutation and can dispatch complementary actions
  */
 
-// Download image when new entries
-observe(model.pokemonList, function (change: IObjectChange) { return; });
+// Fetch live tweets every 30s
+let timer: any;
+reaction(
+    () => model.pokemon,
+    (pokemon: Pokemon) => {
+        if (timer) clearInterval(timer);        
+        timer = setInterval(
+            function () {
+                dispatch("FETCH_TWEETS", normalize(pokemon.name));      
+            },
+            10000
+        );
+        dispatch("FETCH_TWEETS", normalize(pokemon.name));
+    });
